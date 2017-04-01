3ccb7b71730bd8c09c6bb9583b26f02cce4586a0
Leonardo Gentile <leonard.gentile@gmail.com>
2017-04-01 18:17:37 +0200

    Moving away react things

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import routeNode from './modules/routeNode';
 import mobxPlugin from './modules/mobxPlugin';
 import RouterStore from './modules/RouterStore.js';
-import { getComponentFromRoutes } from './modules/utils.js';
 
 export {
   mobxPlugin,
-  RouterStore,
-  routeNode,
-  getComponentFromRoutes
+  RouterStore
 };
