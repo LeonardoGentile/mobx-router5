LeonardoGentile/mobx-router5

commit c60fd24f411fdc54550ba29456408c27f0a7d8bf

perf(RouterStore): Chage the observable routes to be observable.ref
As per discussion on #1 a route should only be changed and emitted by router5. So I now cosiders the
routes exposed by the store are immutable, better to use a reference to the origina object.

BREAKING CHANGE: the whole objects routes: route, previousRoute and transitionRoute are now only
observable reference to the original objects emitted by router5

src/modules/RouterStore.js
@@ -3,9 +3,9 @@ import transitionPath from 'router5.transition-path';
 
 class RouterStore {
 
-  @observable route = null;
-  @observable previousRoute = null;
-  @observable transitionRoute = null;
+  @observable.ref route = null;
+  @observable.ref previousRoute = null;
+  @observable.ref transitionRoute = null;
   @observable transitionError = null;
   @observable intersectionNode = '';
   // @observable currentView;
@@ -20,19 +20,26 @@ class RouterStore {
     this.router = router;
   }
 
+  updateRoute(routeType, route) {
+    this[routeType] = route;
+  }
+
+  resetRoute(routeType) {
+    this[routeType] = null;
+  }
 
   //  ===========
   //  = ACTIONS =
   //  ===========
   // These are called by the plugin
   @action onTransitionStart = (route, previousRoute) => {
-    this.transitionRoute = route;
+    this.updateRoute('transitionRoute', route);
     this.transitionError = null;
   };
 
   @action onTransitionSuccess = (route, previousRoute, opts) => {
-    this.route = route;
-    this.previousRoute = previousRoute;
+    this.updateRoute('route', route);
+    this.updateRoute('previousRoute', previousRoute);
     if (route && !opts.reload) {
       const { intersection } = transitionPath(route, previousRoute);
       this.intersectionNode = intersection;
@@ -41,17 +48,18 @@ class RouterStore {
   };
 
   @action onTransitionCancel = (route, previousRoute) => {
-    this.transitionRoute = '';
+    this.resetRoute('transitionRoute');
   };
 
   @action onTransitionError = (route, previousRoute, transitionError) => {
-    this.transitionRoute = route;
+    this.updateRoute('transitionRoute', route);
+    this.updateRoute('previousRoute', previousRoute);
     this.transitionError = transitionError;
   };
 
   // These can be called manually
   @action clearErrors = () => {
-    this.transitionRoute = null;
+    this.resetRoute('transitionRoute');
     this.transitionError = null;
   };
 

commit 7cd9a955a6b896a6d7ef4860ebcc003290f8662d

revert(RouterStore): Routes objects are now plain observable object again
Revert the breaking change introduced by c60fd24f411fd . That change made all the routes object to
be @observable.ref, but I don't feel comfortable with that. Probably other users can inject things
into the @observable routes and observe those.

src/modules/RouterStore.js
@@ -3,14 +3,13 @@ import transitionPath from 'router5.transition-path';
 
 class RouterStore {
 
-  @observable.ref route = null;
-  @observable.ref previousRoute = null;
-  @observable.ref transitionRoute = null;
+  @observable route = null;
+  @observable previousRoute = null;
+  @observable transitionRoute = null;
   @observable transitionError = null;
   @observable intersectionNode = '';
   @observable toActivate = [];
   @observable toDeactivate = [];
-
   // @observable currentView;
 
   router = null;